9ca350ca6fecda856ad6a2403c7741fdf23c38a1
Aman Reja <amnmolla742@gmail.com>
2025-01-15 18:28:43 +0530

    fixed lag

---
+++ controller/order_controller.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const orderSchema = require("../model/orderSchema");
+const router = express.Router();
+const CartSchema = require("../model/CartSchema");
+
+router.post("/addOrder", async (req, res) => {
+  const { bookname, price, quantity, bookimage, authore, offer } = req.body;
+  const data = new orderSchema({
+    userid: req.body.userid,
+    itemid: req.body.itemid,
+    itemquantity: req.body.itemquantity,
+    items: [{ bookname, price, quantity, bookimage, authore, offer }]
+  });
+
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+
+    const deleteCart = await CartSchema.findByIdAndDelete(req.body.userid);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
